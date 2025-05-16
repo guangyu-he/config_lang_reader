07ee30622343222e83397e92a71c6219ab100496
Guangyu He <guangyu.he@golden-tech.de>
2025-05-16 09:45:43 +0200

    test: add test configuration files for config_lang_reader library

---
+++ tests/py/test.py
@@ -0,0 +1,10 @@
+import config_lang_reader
+
+a = config_lang_reader.read_toml("../test.toml")
+b = config_lang_reader.read_yaml("../test.yaml")
+c = config_lang_reader.read_json("../test.json")
+d = config_lang_reader.read_xml("../test.xml")
+
+e = config_lang_reader.read("../test.toml")
+
+pass
