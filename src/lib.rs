60c68a04d17c3127fd1ed5f2898109d6ab2e74ed
Guangyu He <guangyu.he@golden-tech.de>
2025-05-22 10:30:05 +0200

    refactor(lib): consolidate file reading and writing functions

--- src/lib.rs
+++ src/lib.rs
@@ -8,51 +8,38 @@ use pyo3::prelude::*;
 use pyo3::types::PyDict;
 use std::collections::HashMap;
 
-/// Reads a TOML file and returns its contents as a Python dict.
-#[pyfunction]
-fn read_toml(py: Python<'_>, path: &str) -> PyResult<Py<PyDict>> {
-    toml_lib::toml_to_py(py, path)
-}
-
-/// Reads a YAML file and returns its contents as a Python dict.
-#[pyfunction]
-fn read_yaml(py: Python<'_>, path: &str) -> PyResult<Py<PyDict>> {
-    yaml_lib::yaml_to_py(py, path)
-}
-
-/// Reads a JSON file and returns its contents as a Python dict.
-#[pyfunction]
-fn read_json(py: Python<'_>, path: &str) -> PyResult<Py<PyDict>> {
-    json_lib::json_to_py(py, path)
-}
-
-/// Reads an XML file and returns its contents as a Python dict.
-#[pyfunction]
-fn read_xml(py: Python<'_>, path: &str) -> PyResult<Py<PyDict>> {
-    xml_lib::xml_to_py(py, path)
-}
+use resolve_array_and_table::resolve_table;
 
 /// A General method to read a file and return its contents as a Python dict
 /// based on the file extension.
 #[pyfunction]
 fn read(py: Python<'_>, path: &str) -> PyResult<Py<PyDict>> {
     match path.split('.').last() {
-        Some("toml") => read_toml(py, path),
-        Some("yaml") => read_yaml(py, path),
-        Some("json") => read_json(py, path),
-        Some("xml") => read_xml(py, path),
+        Some("toml") => toml_lib::toml_to_py(py, path),
+        Some("yaml") => yaml_lib::yaml_to_py(py, path),
+        Some("json") => json_lib::json_to_py(py, path),
+        Some("xml") => xml_lib::xml_to_py(py, path),
         _ => Err(pyo3::exceptions::PyValueError::new_err(
             "Unsupported file extension".to_string(),
         )),
     }
 }
 
-/// Writes a TOML file from a Python dictionary.
+/// Writes a Python dictionary to a file based on the file extension.
 #[pyfunction]
-fn write_toml(py: Python<'_>, dict: HashMap<String, PyObject>, path: &str) -> PyResult<()> {
-    let toml_table = resolve_array_and_table::resolve_table(py, dict);
-    let toml_string = toml::to_string(&toml_table).unwrap();
-    std::fs::write(path, toml_string)?;
+fn write(py: Python<'_>, dict: HashMap<String, PyObject>, path: &str) -> PyResult<()> {
+    let toml_table = resolve_table(py, dict);
+    let string = match path.split('.').last() {
+        Some("toml") => toml::to_string(&toml_table).unwrap(),
+        Some("yaml") => serde_yaml::to_string(&toml_table).unwrap(),
+        Some("json") => serde_json::to_string_pretty(&toml_table).unwrap(),
+        _ => {
+            return Err(pyo3::exceptions::PyValueError::new_err(
+                "Unsupported file extension".to_string(),
+            ))
+        }
+    };
+    std::fs::write(path, string)?;
 
     Ok(())
 }
@@ -60,11 +47,7 @@ fn write_toml(py: Python<'_>, dict: HashMap<String, PyObject>, path: &str) -> Py
 /// A Python module implemented in Rust.
 #[pymodule]
 fn config_lang_serder(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(read_toml, m)?)?;
-    m.add_function(wrap_pyfunction!(read_yaml, m)?)?;
-    m.add_function(wrap_pyfunction!(read_json, m)?)?;
-    m.add_function(wrap_pyfunction!(read_xml, m)?)?;
     m.add_function(wrap_pyfunction!(read, m)?)?;
-    m.add_function(wrap_pyfunction!(write_toml, m)?)?;
+    m.add_function(wrap_pyfunction!(write, m)?)?;
     Ok(())
 }
--- tests/py/test.py
+++ tests/py/test.py
@@ -1,15 +1,7 @@
 import config_lang_serder
 
-a = config_lang_serder.read_toml("../test.toml")
+a = config_lang_serder.read("../test.toml")
+config_lang_serder.write(a, "../test_created.json")
 
-# write test
-config_lang_serder.write_toml(a, "../test_created.toml")
-
-b = config_lang_serder.read_yaml("../test.yaml")
-c = config_lang_serder.read_json("../test.json")
-d = config_lang_serder.read_xml("../test.xml")
-
-e = config_lang_serder.read("../test.toml")
-
-fail = config_lang_serder.read("../test.html")  # will throw unsupported file extension
+# fail = config_lang_serder.read("../test.html")  # will throw unsupported file extension
 pass
